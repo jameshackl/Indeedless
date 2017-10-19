fb22ff5773c0f1496152fc7e705b81f02655d811
jameshackl <james.f.hackl@gmail.com>
2017-10-18 22:29:48 -0600

    Init file to prepare storage - initial commit

---
+++ init.js
@@ -0,0 +1,21 @@
+var filterDefaults = [
+    {'archived': true},
+    {'saved': true},
+    {'applied':true},
+    {'visited':false}
+];
+
+var databaseDefault = {'hiddenPostings':[]};
+
+//check to see if a database exists, if not create one. now every other file can assume it exists
+chrome.storage.local.getBytesInUse(function(bytesUsed){
+    if(bytesUsed == 0)
+    {
+        filterDefaults.forEach(function(filter) {
+            chrome.storage.local.set(filter,function(){});
+        }, this);
+        chrome.storage.local.set(databaseDefault,function(){});
+    }
+});
+
+
